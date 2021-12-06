41f81e8256a67e0348c34a26a80a0f9485b8d81e
Thiago Yuri <thiagoyuri0109@gmail.com>
2021-12-05 21:31:25 -0300

    Variable continued 1-2

---
+++ C/Variables/VariablesContinued.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float tenValue = 10.000000;
+	printf("The value of nine is: %f\n", 09.000);
+	printf("The value of ten is: %f\n", tenValue);
+	return 0;
+
+    //Expected output
+    //The value of nine is: 9.000000
+    //The value of ten is: 10.000000
+}
---
+++ C/Variables/VariablesContinued2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	float halfValue = 0.5;
+	float piValue = 3.141593;
+	printf("The value of half is: %f\n", halfValue);
+	printf("The value of Pi is: %f\n", piValue);
+	return 0;
+
+    //Expected output
+    //The value of half is: 0.500000
+    //The value of Pi is: 3.141593
+}
