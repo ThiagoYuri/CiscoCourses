73dfad644b3689e2fd97f3dbd411b4e829341710
Thiago Yuri <thiagoyuri0109@gmail.com>
2021-12-05 22:54:40 -0300

    Floating Point Part 1

---
+++ C/FloatingPoint/FloatingPointPart1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    //%f added value and float type
+    //7. means 7.00000
+	printf("The value of seven is: %f\n", 7.);
+	printf("The value of eight and a half is: %f\n", 8.5);
+	return 0;
+
+
+	//Expected output
+    //The value of seven is: 7.000000
+    //The value of eight and a half is: 8.500000
+}
